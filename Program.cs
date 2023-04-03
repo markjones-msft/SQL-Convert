f3f52949ce68e26914a3046b8fa67d37b1609436
Mark Jones <54671949+markjones-msft@users.noreply.github.com>
2023-04-03 12:47:18 +0100

    Update Program.cs

--- Program.cs
+++ Program.cs
@@ -1,24 +1,4 @@
-﻿/*
-This code reads in SQL code from the console, prompts the user to enter the code and then uses the OpenAI API to convert it to TSQL.
-
-It starts by creating a StringBuilder object to accumulate the SQL code entered by the user. 
-It then enters a loop where it reads in lines of input from the console until the user enters a blank line. 
-Each line of input is added to the StringBuilder object with AppendLine.
-
-After the input is read in, it creates a Prompt string that includes the SQL code entered by the user, 
-and then creates an instance of the OpenAIAPI class using the API key provided.
-
-The code then calls the CreateCompletionAsync method on the API object, passing in a CompletionRequest 
-object that includes the prompt and the name of the model to use (text-davinci-003 in this case).
-
-The API call returns a CompletionResult object that contains a list of Choice objects. 
-The code retrieves the first choice and its Message property, which contains the converted TSQL code.
-
-Finally, the converted TSQL code is printed to the console with Console.WriteLine.
-
-*/
-
-using OpenAI_API;
+﻿using OpenAI_API;
 using OpenAI_API.Completions;
 using System.Text;
 
